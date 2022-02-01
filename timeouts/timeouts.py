afe9f9ef87ecf1d077d4cf523e9932065a0cd1c1
200percentmicky <iiz10ninja@gmail.com>
2022-02-01 04:16:15 -0600

    fix nameerror

--- timeouts/timeouts.py
+++ timeouts/timeouts.py
@@ -4,6 +4,23 @@ import discord
 
 from redbot.core import commands, modlog, checks
 
+# https://stackoverflow.com/questions/70459488/discord-py-timeout-server-members
+# A handshake must be made to Discord since Timeouts aren't implemented in discord.py
+
+async def timeout_user(bot, user_id: int, guild_id: int, until):
+    """
+    Handshake to timeout users in minutes.
+    """
+    headers = {"Authorization": f"Bot {bot.http.token}"}
+    url = f"https://discord.com/api/v9/guilds/{guild_id}/members/{user_id}"
+    timeout = (datetime.datetime.utcnow() + datetime.timedelta(minutes=until)).isoformat()
+    params = {'communication_disabled_until': timeout}
+    async with aiohttp.ClientSession() as session:
+        async with session.patch(url, params=params, headers=headers) as response:
+            if response.status in range(200, 299):
+                return True
+            return False
+
 class Timeouts(commands.Cog):
     """
     Timeouts for Red V3
@@ -28,24 +45,6 @@ class Timeouts(commands.Cog):
             await modlog.register_casetype(**timeout_type)
         except RuntimeError:
             pass
-
-    # https://stackoverflow.com/questions/70459488/discord-py-timeout-server-members
-    # A handshake must be made to Discord since Timeouts aren't implemented in discord.py
-
-    async def timeout_user(bot, user_id: int, guild_id: int, until):
-        """
-        Handshake to timeout users in minutes.
-        """
-        headers = {"Authorization": f"Bot {bot.http.token}"}
-        url = f"https://discord.com/api/v9/guilds/{guild_id}/members/{user_id}"
-        timeout = (datetime.datetime.utcnow() + datetime.timedelta(minutes=until)).isoformat()
-        params = {'communication_disabled_until': timeout}
-        async with aiohttp.ClientSession() as session:
-            async with session.patch(url, params=params, headers=headers) as response:
-                if response.status in range(200, 299):
-                    return True
-                return False
-
 
     @commands.command()
     @checks.has_permissions()
