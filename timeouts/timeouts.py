f01969fc84bc54f751d24a9d1a9b14db78f1cb36
Micky D <iiz10ninja@gmail.com>
2022-02-01 19:03:42 -0600

    replace old method with http method from library

--- timeouts/timeouts.py
+++ timeouts/timeouts.py
@@ -7,19 +7,13 @@ from redbot.core import commands, modlog, checks
 # https://stackoverflow.com/questions/70459488/discord-py-timeout-server-members
 # A handshake must be made to Discord since Timeouts aren't implemented in discord.py
 
-async def timeout_user(bot, user_id: int, guild_id: int, until):
+async def timeout_user(bot, user_id: int, guild_id: int, reason: str, until):
     """
     Handshake to timeout users in minutes.
     """
-    headers = {"Authorization": f"Bot {bot.http.token}"}
-    url = f"https://discord.com/api/v9/guilds/{guild_id}/members/{user_id}"
     timeout = (datetime.datetime.utcnow() + datetime.timedelta(minutes=until)).isoformat()
     params = {'communication_disabled_until': timeout}
-    async with aiohttp.ClientSession() as session:
-        async with session.patch(url, params=params, headers=headers):
-            if session.status in range(200, 299):
-                return True
-            return False
+    await bot.http.edit_member(guild_id, user_id, reason, **until)
 
 class Timeouts(commands.Cog):
     """
@@ -57,10 +51,11 @@ class Timeouts(commands.Cog):
         `[reason]` The reason for the timeout.
         """
         handshake = await timeout_user(self.bot, user_id=member.id, guild_id=ctx.guild.id, until=until)
-        if handshake:
-            await modlog.create_case(
-                ctx.bot, ctx.guild, ctx.message.created_at, action_type="timeout",
-                user=member, moderator=ctx.author, reason=reason
-            )
-            await ctx.send(f"Done. They'll be back in **{until}** minutes.")
-        await ctx.send("Something went wrong")
+        async with ctx typing():
+            if handshake:
+                await modlog.create_case(
+                    ctx.bot, ctx.guild, ctx.message.created_at, action_type="timeout",
+                    user=member, moderator=ctx.author, reason=reason
+                )
+                await ctx.send(f"Done. They'll be back in **{until}** minutes.")
+            await ctx.send("Something went wrong")
