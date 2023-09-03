16d541e1959f43deef8273497cd07b245069e736
Micky D <iiz10ninja@gmail.com>
2023-09-03 04:01:19 -0500

    Update lewdpolice.py

--- lewdpolice/lewdpolice.py
+++ lewdpolice/lewdpolice.py
@@ -3,6 +3,8 @@ import discord
 from redbot.core import commands, config
 from nsfw_detector import predict
 
+# Currently a work in progress...
+
 class LewdPolice(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
